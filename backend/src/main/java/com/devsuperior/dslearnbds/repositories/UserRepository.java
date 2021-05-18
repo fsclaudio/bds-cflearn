9f151a3ca55f11ab7441fa4713ee4bd45fb00468
fsclaudio <fsclaudio@gmail.com>
2021-05-18 18:36:50 -0300

    Oauth security, login

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.devsuperior.dslearnbds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dslearnbds.entities.Role;
+import com.devsuperior.dslearnbds.entities.User;
 
-public interface UserRepository extends JpaRepository<Role, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
 
 }
